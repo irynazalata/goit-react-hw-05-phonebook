04da4190d7c07f18305ab9c01ffa0e1af80e785b
Iryna Zalata <iryna.zalata@gmail.com>
2020-12-08 16:19:42 +0200

    Added completed hometask

--- src/App.js
+++ src/App.js
@@ -6,24 +6,27 @@ import Filter from './components/Filter/Filter';
 import Notification from './shared/Notification/Notification';
 import './App.css';
 
+const newContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
     contactExists: false,
     existingName: '',
   };
 
   componentDidMount() {
-    const persistedContacts = localStorage.getItem('contacts');
+    const persistedContacts =
+      JSON.parse(localStorage.getItem('contacts')) || newContacts;
     if (persistedContacts) {
       this.setState({
-        contacts: JSON.parse(persistedContacts),
+        contacts: persistedContacts,
       });
     }
   }
